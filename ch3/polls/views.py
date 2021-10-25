8fc72eb9845d029405ef0bef1d7e6177677f41f7
psb6396 <psb6396@naver.com>
2021-10-25 17:18:33 +0900

    ff

--- ch3/polls/views.py
+++ ch3/polls/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import get_object_or_404,render
-from polls.models import Question 
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from polls.models import Choice, Question 
+
 
 def index(request):
     latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
@@ -10,3 +13,23 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk = question_id)
     return render(request, 'polls/detail.html', {'question':question})
 
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        #설문 투표 폼을 다시 보여준다
+        return render(request, 'polls/detail.html', {
+            'question' : question,
+            'error_message' : "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # POST 데이터를 정상적으로 처리하였으면,
+        # 항상 HttpResponseRedirect를 반환하여 리다이렉션 처리함
+        return HttpResponseRedirect(reverse('polls:results', args = (question.id,)))
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/results.html', {'question': question})
