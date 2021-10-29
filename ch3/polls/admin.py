0e918533bd61aa70c0d9c91c568e5eb77b462f86
psb6396 <psb6396@naver.com>
2021-10-29 16:48:21 +0900

    sdd

--- ch3/polls/admin.py
+++ ch3/polls/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields' : ['question_text']}),
+        ('Date information', {'fields' : ['pub_date'], 'classes' : ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
